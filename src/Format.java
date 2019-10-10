8e9e27b1d8fa836356fe1050252370ab1d529fd5
MartinKalchev <54631309+MartinKalchev@users.noreply.github.com>
2019-10-10 17:18:28 +0300

    Update Format.java

--- src/Format.java
+++ src/Format.java
@@ -1,4 +1,4 @@
-
+// this is a enum class for the paper format of the items printed in the shop listing the different prices for each format
 public enum Format {
 
 	A1(0.05),A2(0.10),A3(0.15),A4(0.25),A5(0.30);
