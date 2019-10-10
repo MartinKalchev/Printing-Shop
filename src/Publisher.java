819e338429bf264a0872a88bb34584b005d626a5
MartinKalchev <54631309+MartinKalchev@users.noreply.github.com>
2019-10-10 17:30:54 +0300

    Update Publisher.java

--- src/Publisher.java
+++ src/Publisher.java
@@ -68,10 +68,11 @@ public class Publisher {
 		machines.add(machine);
 	}
 	
-	
+	// function that adds a bonus to the base salaries of the employees if the revenue of the shop satisfies a given target
 	public void bonus_sal(double bonus) {
 		double extended_sal = 0;
 		double revenue_for_bonus = 200;
+		// checks if the target revenue is reached
 		if(this.revenue() > revenue_for_bonus) {
 			Employee.MANAGER.base_sal = Employee.MANAGER.getBase_sal() * (1+bonus);
 		}else {
@@ -80,6 +81,7 @@ public class Publisher {
 		
 	}
 	
+	// function that calculates the total salary expediture for the shop
 	public double totalSalCost() {
 		double total = 0;
 		for(Employee e: employees) {
@@ -89,6 +91,7 @@ public class Publisher {
 		return total;
 	}
 	
+	// function that calculates the total price (expenditure) for all the printed items for the shop
 	public double TotalPaperPrice() {
 		double total = 0;
 		for(Publishing p : publishings) {
@@ -97,6 +100,7 @@ public class Publisher {
 			return total;	
 	}
 	
+	// function to calculate the selling price of the item which is printed
 	public double TotalPaperSellPrice() {
 		double total = 0;
 		double per = .60;
@@ -107,7 +111,7 @@ public class Publisher {
 	}
 	
 	
-	
+	// function that computes the total costs of the printing shop
 	public double totalCosts() {
 		double total = 0;
 		total += (this.totalSalCost() + this.TotalPaperPrice());
@@ -115,7 +119,7 @@ public class Publisher {
 	}
 	
 	
-	
+	// function the computes the total revenue of the shop
 	public double revenue() {
 		double total = 0;
 				if(num_publishings > 4){
@@ -128,7 +132,7 @@ public class Publisher {
 	
 	}
 	
-    	
+    	// the main thread function which makes the two printing machines print the items simultaneously
 	 public void publish() {
 			for(PrintingMachine m : machines) {
 				Runnable runnable = new Runnable() {
